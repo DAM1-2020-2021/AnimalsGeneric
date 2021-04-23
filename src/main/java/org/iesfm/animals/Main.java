cd7b55ed96b346a02eb3d41380b46aeffd72274c
Mikel <mikelsanvi@gmail.com>
2021-04-23 12:30:43 +0200

    added mouse

--- src/main/java/org/iesfm/animals/Main.java
+++ src/main/java/org/iesfm/animals/Main.java
@@ -3,9 +3,10 @@ package org.iesfm.animals;
 public class Main {
 
     public static void main(String[] args) {
-        // NO COMPILA Animal<Str> str = new Animal("black", 3, "hola");
+        // Animal<String> str = new Animal("black", 3, "hola"); // no compila por culpa del type bound
 
         Animal<Cat> cat = new Animal("black", 3, new Cat(7));
+        Animal<Mouse> mouse = new Animal("black", 3, new Mouse());
 
         cat.getSpecie().jump(4);
     }
---
+++ src/main/java/org/iesfm/animals/Mouse.java
@@ -0,0 +1,9 @@
+package org.iesfm.animals;
+
+public class Mouse implements Specie {
+
+    @Override
+    public void saySomething() {
+        System.out.println("iiiii");
+    }
+}
